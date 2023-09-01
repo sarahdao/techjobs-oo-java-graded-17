f9e13f878e91c9be5875d9226cef9afaa4ded405
Sarah Dao <sarahdao@Sarahs-MBP-3.attlocal.net>
2023-09-01 12:21:33 -0500

    completed task two

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -11,7 +11,13 @@ public class Location {
     public Location() {
         id = nextId;
         nextId++;
+
     }
+    public Location(String value){
+        this();
+        this.value = value;
+    }
+
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -16,10 +18,25 @@ public class PositionType {
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    public String toString(){
+        return value;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType that)) return false;
+        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue());
+    }
 
     // Getters and Setters:
 
