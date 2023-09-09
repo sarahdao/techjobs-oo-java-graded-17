47b3262f7d550a69453e9e9ffbf07e0af58ef468
Sarah Dao <sarahdao@Sarahs-MacBook-Pro-3.local>
2023-09-09 13:57:55 -0500

    added JobTest

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -16,19 +16,19 @@ public class Job {
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
-public Job(){
-    id = nextId;
-    nextId++;
-}
+    public Job(){
+        id = nextId;
+        nextId++;
+    }
 
-public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
-    this();
-    this.name = name;
-    this.employer = employer;
-    this.location = location;
-    this.positionType = positionType;
-    this.coreCompetency = coreCompetency;
-}
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
@@ -89,5 +89,8 @@ public Job(String name, Employer employer, Location location, PositionType posit
     @Override
     public int hashCode() {
         return Objects.hash(getId());
+    }
+
+    public void setId(int i) {
     }
 }
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,13 @@
 package org.launchcode.techjobs.oo;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class JobTest {
-    //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        job1.setId(1);
+
+        assertEquals(1, job1.getId());
+    }
 }
